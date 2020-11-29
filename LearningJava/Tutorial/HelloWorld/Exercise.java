a33064a419065589208d1069aa95b14df71847f7
Fabian Gal <fabiangal.45@gmail.com>
2020-11-29 23:16:38 +0000

    Attempt at the exercise

--- LearningJava/Tutorial/HelloWorld/Exercise.java
+++ LearningJava/Tutorial/HelloWorld/Exercise.java
@@ -10,11 +10,31 @@ public class Exercise {
         //Mortgage : have to calculat this also displayed as a currency
 
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Principal:");
-        // I have to leave this for another day 1:31:26
+        System.out.print("Principal: ");
+        double principal = scanner.nextDouble();
 
+        System.out.print("Annual interest rate: ");
+        double interest = scanner.nextDouble();
+
+        System.out.print("Period in years: ");
+        double period = scanner.nextDouble(); // Number of payments |I would like to introduce byte here 
+
+        double r = (interest/100)/12; // monthly interest rate
+
+        double n = period * 12; 
+
+        double total = principal * (r*Math.pow((1+r), n)/Math.pow((1+r), n) -1);
+        
+        System.out.println(total);
+        
         scanner.close();
 
 
+        // double total = Math.pow(principal, interest); // raising to the power
+
+        // System.out.println(principal); // tested the inputs
+        // System.out.println(interest); // tested the inputs
+        // System.out.println(Math.round(principal) + "^" + interest + " = " + total); //tested the input
+        
     }
 }
