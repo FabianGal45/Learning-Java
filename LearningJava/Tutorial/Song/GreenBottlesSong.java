fc49c0987d8f1ab38c04c86c09f789f3fe483437
Fabian Gal <fabiangal.45@gmail.com>
2020-12-04 01:28:10 +0000

    Broken Prototype

--- LearningJava/Tutorial/Song/GreenBottlesSong.java
+++ LearningJava/Tutorial/Song/GreenBottlesSong.java
@@ -2,22 +2,17 @@ package LearningJava.Tutorial.Song;
 
 public class GreenBottlesSong {
     public static void main(String[] args) {
-        String[] bottles = {"Ten", "Nine", "Eight", "Seven", "Six", "Five", "Four", "Three", "Two"};
-         //bottles[i+1].toLowerCase();
+        String[] bottles = {"Ten", "Nine", "Eight", "Seven", "Six", "Five", "Four", "Three", "Two", "One"};
 
-        // System.out.println(bottlesLeft);
         for (int i = 0; i <= bottles.length; i++) {
           String bottlesLeft = bottles[i+1].toLowerCase();
           System.out.println(bottles[i] + " green bottles hanging on the wall.");
           System.out.println(bottles[i] + " green bottles hanging on the wall.");
           System.out.println("And if one green bottle, should accidentally fall.");
-          System.out.println("There'll be " + bottlesLeft +" green bottles, hanging on the wall.");
-          System.out.println("");
-          // System.out.println(cars[i+])
+          System.out.println("There'll be " + bottlesLeft +" green bottles, hanging on the wall.\n");
         }
-        for (int r = 0; r <= 1; r++) {
-          System.out.println("One green bottle hanging on the wall.");
-        }
+        System.out.println("One green bottle hanging on the wall.");
+        System.out.println("One green bottle hanging on the wall.");
         System.out.println("And if that green bottle, should accidentally fall.");
         System.out.println("There'll be no green bottles, hanging on the wall.");
 
