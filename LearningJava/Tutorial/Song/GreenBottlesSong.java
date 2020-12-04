7cf79d52fa0c1b63314b58c47ae3f4513dbd7347
Fabian Gal <fabiangal.45@gmail.com>
2020-12-04 01:07:23 +0000

    Working prototype

--- LearningJava/Tutorial/Song/GreenBottlesSong.java
+++ LearningJava/Tutorial/Song/GreenBottlesSong.java
@@ -3,18 +3,25 @@ package LearningJava.Tutorial.Song;
 public class GreenBottlesSong {
     public static void main(String[] args) {
         String[] bottles = {"Ten", "Nine", "Eight", "Seven", "Six", "Five", "Four", "Three", "Two"};
-        for (String i : bottles) {
-          System.out.println(i + " green bottles hanging on the wall.");
-          System.out.println(i + " green bottles hanging on the wall.");
+         //bottles[i+1].toLowerCase();
+
+        // System.out.println(bottlesLeft);
+        for (int i = 0; i <= bottles.length; i++) {
+          String bottlesLeft = bottles[i+1].toLowerCase();
+          System.out.println(bottles[i] + " green bottles hanging on the wall.");
+          System.out.println(bottles[i] + " green bottles hanging on the wall.");
           System.out.println("And if one green bottle, should accidentally fall.");
-          System.out.println("There'll be -1 green bottles, hanging on the wall.");
+          System.out.println("There'll be " + bottlesLeft +" green bottles, hanging on the wall.");
           System.out.println("");
-          if (i == "Two") {
-              System.out.println("One green bottle hanging on the wall.");
-              System.out.println("One green bottle hanging on the wall.");
-              System.out.println("And if that green bottle, should accidentally fall.");
-              System.out.println("There'll be no green bottles, hanging on the wall.");
-            }
+          // System.out.println(cars[i+])
         }
+        for (int r = 0; r <= 1; r++) {
+          System.out.println("One green bottle hanging on the wall.");
+        }
+        System.out.println("And if that green bottle, should accidentally fall.");
+        System.out.println("There'll be no green bottles, hanging on the wall.");
+
+        //Probably change this with if i=Two...
+
       }
 }
